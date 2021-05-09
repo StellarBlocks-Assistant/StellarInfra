57d93402cffd14a1b47a316d6806d756c5743ea7
Jin Dou <SamwiseDou@outlook.com>
2021-05-09 01:31:20 -0400

    add dict for json IO

--- StellarInfra/IO.py
+++ StellarInfra/IO.py
@@ -4,11 +4,23 @@ Created on Wed Jun 17 12:07:00 2020
 
 @author: Jin Dou
 """
+import json
 import array,os
 import numpy as np
 from scipy import io as scipyIO
 from .DirManage import checkFolder
+from functools import singledispatch
 
+
+@singledispatch
+def toJson(val):
+    """ default for json"""
+    return val
+
+@toJson.register(np.float32)
+def toJson_float32(val):
+    """ for np float 32"""
+    return np.float64(val)
 
 ''' Python Object IO'''
 def saveObject(Object,folderName,tag, ext = '.bin'):
@@ -24,6 +36,15 @@ def loadObject(filePath):
     temp = pickle.load(file)
     return temp
 
+def saveDictJson(filePath:str,Obj:dict):
+    with open(filePath,'w') as file:
+        json.dump(Obj,file,default = toJson)
+        
+def loadJson(filePath:str):
+    out = None
+    with open(filePath) as json_file:
+        out = json.load(json_file)
+    return out
 ''' End '''
 
 ''' Load Bin and Text File '''
